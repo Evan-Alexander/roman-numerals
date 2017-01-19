a383826dc1dff99542d06bc7f321e5e126f1ea9e
Charles Peden and Jason Brown <jason@>
2017-01-19 11:08:16 -0800

    build result string logic, null still problematic

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,7 @@ var M = 1000;
 var ones = [null, "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
 var tens = [null, "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
 var hundreds = [null, "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+var thousands = [null, "M", "MM", "MMM"]
 
 var validateInput = function(numVar) {
   if (isNaN(numVar)) {
@@ -37,23 +38,40 @@ var onesIdentifier = function(numbers) {
 }
 
 var tensIdentifier = function(numbers) {
+  if (numbers.length > 1) {
   var tensPlace = numbers[numbers.length - 2];
   return tens[tensPlace];
+  } else {
+    return null;
+  }
 }
 
 var hundredsIdentifier = function(numbers) {
+  if (numbers.length > 2) {
   var hundredsPlace = numbers[numbers.length - 3];
   return hundreds[hundredsPlace];
-}
+  } else {
+    return null;
+  };
+};
 
+var thousandsIdentifier = function(numbers) {
+  if (numbers.length > 3) {
+  var thousandsPlace = numbers[numbers.length - 4];
+  return thousands[thousandsPlace];
+  } else {
+    return null;
+  };
+};
 
-
-
-
-
-
-
-
+var roman = function(thousandsPlace, hundredsPlace, tensPlace, onesPlace){
+  var romani = [thousandsPlace, hundredsPlace, tensPlace, onesPlace]
+  console.log(romani);
+  romani = romani.filter(function(roman){
+    return roman;
+    console.log(romani);
+  });
+};
 
 
 
@@ -69,10 +87,12 @@ $(document).ready(function() {
     var numbers = numberSplitter(verified);
     var onesPlace = onesIdentifier(numbers);
     var tensPlace = tensIdentifier(numbers);
-    var hundredsPlace = hundredsIdentifier(numbers);
-    console.log(hundredsPlace + tensPlace + onesPlace);
-//    console.log(numbers);
-//    var result = roman(number);
+    // console.log(onesPlace);
+   var hundredsPlace = hundredsIdentifier(numbers);
+   var thousandsPlace = thousandsIdentifier(numbers);
+//   console.log(thousandsPlace + hundredsPlace + tensPlace + onesPlace);
+   var result = roman(thousandsPlace, hundredsPlace, tensPlace, onesPlace);
+   console.log(result);
     $(".number").text(number);
     $(".roman").text(result);
 
